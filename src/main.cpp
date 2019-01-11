689d6748a5067c48675c048fdd65fb9a22b77c15
christiancazu@gmail.com <30902930+christiancazu@users.noreply.github.com>
2019-01-11 14:03:34 -0500

    format, set, and print Matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,74 @@
-#include <iostream>
-#include <conio.h>
+#include <iostream>			/* input, output */
+#include <conio.h>			/* getch */
+#include <stdlib.h>			/* srand, rand */
+#include <time.h>				/* time */
+#include <stdlib.h>			/* new, delete pointers buffer */
 
 using namespace std;
 
-int main() {
-	cout << "Quickstart C++ project" << endl;
-	getch(); 
+#pragma region Global variables(declaration)
+
+int rows, columns, **matrix;				/* matrix params */
+int minRange = 0, maxRange = 9;			/* range for random numbers */
+
+#pragma endregion
+
+#pragma region Function prototype(declaration)
+
+void formatMatrix();			/* show matrix values */
+void setMatrix();					/* setting matrix values */
+void printMatrix();				/* initialize matrix dimentions */
+
+#pragma endregion
+
+int main()
+{
+	formatMatrix();
+	setMatrix();
+	printMatrix();
+
+	delete[] matrix;
+	getch();
 	return 0;
 }
+
+#pragma region Functions
+
+void printMatrix()
+{
+
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < columns; j++)
+		{
+			cout << *(*(matrix + i) + j) << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void formatMatrix()
+{
+
+	cout << "Indique cantidad de filas: " << '\n';
+	cin >> rows;
+	cout << "Indique cantidad de columnas: " << '\n';
+	cin >> columns;
+
+	matrix = new int *[rows];
+
+	for (size_t i = 0; i < rows; i++)
+		matrix[i] = new int[columns];
+}
+
+void setMatrix()
+{
+
+	srand(time(NULL));
+
+	for (size_t i = 0; i < rows; i++)
+		for (size_t j = 0; j < columns; j++)
+			*(*(matrix + i) + j) = rand() % (maxRange - minRange + 1) + minRange;
+}
+
+#pragma endregion
