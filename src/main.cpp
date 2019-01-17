88eb8a5695d690b70a12e0648f1a7343a1deb6b5
christiancazu@gmail.com <30902930+christiancazu@users.noreply.github.com>
2019-01-16 23:49:37 -0500

    setting tree with nodes
    TODO: ways to solutions

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,55 @@
-#include <iostream>			/* input, output */
-#include <conio.h>			/* getch */
-#include <stdlib.h>			/* srand, rand */
-#include <time.h>				/* time */
-#include <stdlib.h>			/* new, delete pointers buffer */
+#include <iostream>			// input, output
+#include <stdlib.h>			// srand, rand
+#include <time.h>				// time
+#include <stdlib.h>			// new, delete pointers buffer
 
 using namespace std;
 
 #pragma region Global variables(declaration)
 
-int rows, columns, **matrix;				/* matrix params */
-int minRange = 0, maxRange = 9;			/* range for random numbers */
+struct Node
+{
+	int value;
+	Node *right;
+	Node *down;
+};
+
+int rows, columns, **matrix;	// matrix params
+int minRange = 0, maxRange = 9;		// range for random numbers
+Node *tree = NULL;
 
 #pragma endregion
 
 #pragma region Function prototype(declaration)
 
-void formatMatrix();			/* show matrix values */
-void setMatrix();					/* setting matrix values */
-void printMatrix();				/* initialize matrix dimentions */
+void setMatrixFormat();	// show matrix values
+void setMatrixValues();	// setting matrix values
+void printMatrix();			// initialize matrix dimentions
+Node *makeNode(int);		// node maker				
+void setTree();					// setter tree
+void printRoad(Node*);	
 
 #pragma endregion
 
 int main()
 {
-	formatMatrix();
-	setMatrix();
+	setMatrixFormat();
+	setMatrixValues();
+	cout << "\nmatrix :" << '\n';
 	printMatrix();
+	cout << "\nsetting tree with nodes :" << '\n';
+	setTree();
+	//printRoad(tree);
 
 	delete[] matrix;
-	getch();
+	system("pause");
 	return 0;
 }
 
-#pragma region Functions
+#pragma region Functions definition
 
 void printMatrix()
 {
-
 	for (size_t i = 0; i < rows; i++)
 	{
 		for (size_t j = 0; j < columns; j++)
@@ -47,9 +60,8 @@ void printMatrix()
 	}
 }
 
-void formatMatrix()
+void setMatrixFormat()
 {
-
 	cout << "Indique cantidad de filas: " << '\n';
 	cin >> rows;
 	cout << "Indique cantidad de columnas: " << '\n';
@@ -57,18 +69,79 @@ void formatMatrix()
 
 	matrix = new int *[rows];
 
-	for (size_t i = 0; i < rows; i++)
+	for (size_t i = 0; i < rows; i++) {
 		matrix[i] = new int[columns];
+	}
+
 }
 
-void setMatrix()
+void setMatrixValues()
 {
-
 	srand(time(NULL));
-
+	
 	for (size_t i = 0; i < rows; i++)
-		for (size_t j = 0; j < columns; j++)
+		for (size_t j = 0; j < columns; j++) 
 			*(*(matrix + i) + j) = rand() % (maxRange - minRange + 1) + minRange;
+}
+
+
+Node *makeNode(int value) 
+{
+	Node *node = new Node();
+	node->value = value;
+	node->right = NULL;
+	node->down = NULL;
+
+	return node;
+}
+
+void setTree() 
+{
+	// initialize tree
+	tree = makeNode(**matrix);
+	Node *aux;
+	aux = tree;
+	
+	for (size_t r = 0; r < rows; r++)
+	{
+		for (size_t c = 0; c < columns; c++)
+		{
+			if (c < columns - 1)
+			{
+				aux->right = makeNode(matrix[r][c + 1]);
+			}
+			if (r < rows - 1)
+			{
+				aux->down = makeNode(matrix[r + 1][c]);
+			}
+			cout << "\ncurrent node: " << aux->value;
+			if(aux->right != NULL)
+				cout << " to-right: " << aux->right->value;
+			else cout << " to-right:  ";
+			if(aux->down != NULL)
+				cout << " to-down: " << aux->down->value;
+			else cout << " to-down:  ";
+			if(aux->right != NULL)
+			aux = aux->right;	
+		}
+		aux = tree->down;
+		cout << "\n-----------------------\n";
+		for(size_t z = 0; z < r; z++)
+			aux = aux->down;
+	}
+} 
+
+void printRoad(Node *tree) {
+	if(tree == NULL) {
+		return;
+	}	
+	else {
+			//cout<< "\ncurrent tree " << tree->value;
+			printRoad(tree->down);	
+			printRoad(tree->right);			
+	}
+	//cout<< "\ntree finish" << tree->value;
+	return;
 }
 
 #pragma endregion
